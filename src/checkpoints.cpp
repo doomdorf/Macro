6462cfe0720372c93c39300880070bc058599043
doomdorf <simon.karakas1@gmail.com>
2016-09-01 23:11:20 +0200

    Initial Commit

---
+++ src/checkpoints.cpp
@@ -0,0 +1,93 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "txdb.h"
+#include "main.h"
+#include "uint256.h"
+
+
+static const int nCheckpointSpan = 500;
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+
+
+    // macrocoin::: => fix checkpoints
+
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+
+        ( 0,   uint256("0xd32d9b4846422e435015f58edeb3231048df24d28d46b47bdaca52f18d3cf9af") ) // genesis
+        ( 800, uint256("0x098df15bde3e6dafda441056727bbe33121ccf46fe309850594e5cec72d43b69") ) // last pow block
+
+    ;
+
+    // TestNet has no checkpoints
+    static MapCheckpoints mapCheckpointsTestnet;
+
+    bool CheckHardened(int nHeight, const uint256& hash)
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
+        if (i == checkpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        if (checkpoints.empty())
+            return 0;
+        return checkpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, checkpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+
+    // Automatically select a suitable sync-checkpoint
+    const CBlockIndex* AutoSelectSyncCheckpoint()
+    {
+        const CBlockIndex *pindex = pindexBest;
+        // Search backward for a block within max span and maturity window
+        while (pindex->pprev && pindex->nHeight + nCheckpointSpan > pindexBest->nHeight)
+            pindex = pindex->pprev;
+        return pindex;
+    }
+
+    // Check against synchronized checkpoint
+    bool CheckSync(int nHeight)
+    {
+        const CBlockIndex* pindexSync = AutoSelectSyncCheckpoint();
+
+        if (nHeight <= pindexSync->nHeight)
+            return false;
+        return true;
+    }
+}
---
+++ src/qt/overviewpage.cpp
@@ -0,0 +1,251 @@
+#include "overviewpage.h"
+#include "ui_overviewpage.h"
+
+#include "clientmodel.h"
+#include "walletmodel.h"
+#include "bitcoinunits.h"
+#include "optionsmodel.h"
+#include "transactiontablemodel.h"
+#include "transactionfilterproxy.h"
+#include "guiutil.h"
+#include "guiconstants.h"
+
+#include <QAbstractItemDelegate>
+#include <QPainter>
+
+#define DECORATION_SIZE2 29
+#define DECORATION_SIZE 64
+#define NUM_ITEMS 6
+
+class TxViewDelegate : public QAbstractItemDelegate
+{
+    Q_OBJECT
+public:
+    TxViewDelegate(): QAbstractItemDelegate(), unit(BitcoinUnits::BTC)
+    {
+
+    }
+
+    inline void paint(QPainter *painter, const QStyleOptionViewItem &option,
+                      const QModelIndex &index ) const
+    {
+        painter->save();
+
+        QIcon icon = qvariant_cast<QIcon>(index.data(Qt::DecorationRole));
+        QRect mainRect = option.rect;
+        QRect decorationRect(mainRect.topLeft(), QSize(DECORATION_SIZE, DECORATION_SIZE));
+        int xspace = DECORATION_SIZE + 8;
+        int ypad = 6;
+        int halfheight = (mainRect.height() - 2*ypad)/2;
+        QRect amountRect(mainRect.left() + xspace, mainRect.top()+ypad, mainRect.width() - xspace, halfheight);
+        QRect addressRect(mainRect.left() + xspace, mainRect.top()+ypad+halfheight, mainRect.width() - xspace, halfheight);
+        icon.paint(painter, decorationRect);
+
+        QDateTime date = index.data(TransactionTableModel::DateRole).toDateTime();
+        QString address = index.data(Qt::DisplayRole).toString();
+        qint64 amount = index.data(TransactionTableModel::AmountRole).toLongLong();
+        bool confirmed = index.data(TransactionTableModel::ConfirmedRole).toBool();
+        QVariant value = index.data(Qt::ForegroundRole);
+        QColor foreground = option.palette.color(QPalette::Text);
+        if(qVariantCanConvert<QColor>(value))
+        {
+            foreground = qvariant_cast<QColor>(value);
+        }
+
+        painter->setPen(fUseSTheme ? QColor(255, 255, 255) : foreground);
+        painter->drawText(addressRect, Qt::AlignLeft|Qt::AlignVCenter, address);
+
+        if(amount < 0)
+        {
+            foreground = COLOR_NEGATIVE;
+        }
+        else if(!confirmed)
+        {
+            foreground = COLOR_UNCONFIRMED;
+        }
+        else
+        {
+            foreground = option.palette.color(QPalette::Text);
+        }
+        painter->setPen(fUseSTheme ? QColor(255, 255, 255) : foreground);
+        QString amountText = BitcoinUnits::formatWithUnit(unit, amount, true);
+        if(!confirmed)
+        {
+            amountText = QString("[") + amountText + QString("]");
+        }
+        painter->drawText(amountRect, Qt::AlignRight|Qt::AlignVCenter, amountText);
+
+        painter->setPen(fUseSTheme ? QColor(96, 101, 110) : option.palette.color(QPalette::Text));
+        painter->drawText(amountRect, Qt::AlignLeft|Qt::AlignVCenter, GUIUtil::dateTimeStr(date));
+
+        painter->restore();
+    }
+
+    inline QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
+    {
+        return QSize(DECORATION_SIZE, DECORATION_SIZE);
+    }
+
+    int unit;
+
+};
+#include "overviewpage.moc"
+
+OverviewPage::OverviewPage(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::OverviewPage),
+    clientModel(0),
+    walletModel(0),
+    currentBalance(-1),
+    currentStake(0),
+    currentUnconfirmedBalance(-1),
+    currentImmatureBalance(-1),
+    txdelegate(new TxViewDelegate()),
+    filter(0)
+{
+    ui->setupUi(this);
+
+    // Recent transactions
+//   ui->tableViewRecentTransactions->setItemDelegate(txdelegate);
+   //ui->tableViewRecentTransactions->setIconSize(QSize(DECORATION_SIZE2, DECORATION_SIZE2));
+   ui->tableViewRecentTransactions->setMinimumHeight(NUM_ITEMS * (DECORATION_SIZE2));
+   ui->tableViewRecentTransactions->setAttribute(Qt::WA_MacShowFocusRect, false);
+
+    connect(ui->tableViewRecentTransactions, SIGNAL(clicked(QModelIndex)), this, SLOT(handleTransactionClicked(QModelIndex)));
+
+    // init "out of sync" warning labels
+    ui->labelWalletStatus->setText("(" + tr("out of sync") + ")");
+    ui->labelTransactionsStatus->setText("(" + tr("out of sync") + ")");
+
+    // start with displaying the "out of sync" warnings
+    showOutOfSyncWarning(true);
+
+    if (fUseSTheme)
+    {
+        const char* whiteLabelQSS = "QLabel { color: rgb(255,255,255); }";
+        ui->labelBalance->setStyleSheet(whiteLabelQSS);
+        ui->labelStake->setStyleSheet(whiteLabelQSS);
+        ui->labelUnconfirmed->setStyleSheet(whiteLabelQSS);
+        ui->labelImmature->setStyleSheet(whiteLabelQSS);
+        ui->labelTotal->setStyleSheet(whiteLabelQSS);
+    }
+}
+
+void OverviewPage::handleTransactionClicked(const QModelIndex &index)
+{
+    if(filter)
+        emit transactionClicked(filter->mapToSource(index));
+}
+
+OverviewPage::~OverviewPage()
+{
+    delete ui;
+}
+
+void OverviewPage::setBalance(qint64 balance, qint64 stake, qint64 unconfirmedBalance, qint64 immatureBalance)
+{
+    int unit = walletModel->getOptionsModel()->getDisplayUnit();
+    currentBalance = balance;
+    currentStake = stake;
+    currentUnconfirmedBalance = unconfirmedBalance;
+    currentImmatureBalance = immatureBalance;
+    ui->labelBalance->setText(BitcoinUnits::formatWithUnit(unit, balance));
+    ui->labelStake->setText(BitcoinUnits::formatWithUnit(unit, stake));
+    ui->labelUnconfirmed->setText(BitcoinUnits::formatWithUnit(unit, unconfirmedBalance));
+    ui->labelImmature->setText(BitcoinUnits::formatWithUnit(unit, immatureBalance));
+    ui->labelTotal->setText(BitcoinUnits::formatWithUnit(unit, balance + stake + unconfirmedBalance + immatureBalance));
+
+    // only show immature (newly mined) balance if it's non-zero, so as not to complicate things
+    // for the non-mining users
+    bool showImmature = immatureBalance != 0;
+    ui->labelImmature->setVisible(showImmature);
+    ui->labelImmatureText->setVisible(showImmature);
+}
+
+void OverviewPage::setClientModel(ClientModel *model)
+{
+    this->clientModel = model;
+    if(model)
+    {
+        // Show warning if this is a prerelease version
+        connect(model, SIGNAL(alertsChanged(QString)), this, SLOT(updateAlerts(QString)));
+        updateAlerts(model->getStatusBarWarnings());
+    }
+}
+
+void OverviewPage::setWalletModel(WalletModel *model)
+{
+    this->walletModel = model;
+    if(model && model->getOptionsModel())
+    {
+
+        // Set up transaction list
+                /*
+                filter = new TransactionFilterProxy();
+                filter->setSourceModel(model->getTransactionTableModel());
+                filter->setLimit(NUM_ITEMS);
+                filter->setDynamicSortFilter(true);
+                filter->setSortRole(Qt::EditRole);
+                filter->setShowInactive(false);
+                filter->sort(TransactionTableModel::Status, Qt::DescendingOrder);
+                */
+
+
+                TransactionFilterProxy* transactionProxyModel = new TransactionFilterProxy(this);
+                transactionProxyModel->setSourceModel(model->getTransactionTableModel());
+                transactionProxyModel->setDynamicSortFilter(true);
+                transactionProxyModel->setSortCaseSensitivity(Qt::CaseInsensitive);
+                transactionProxyModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+
+                transactionProxyModel->setSortRole(Qt::EditRole);
+
+                ui->tableViewRecentTransactions->setModel(transactionProxyModel);
+                ui->tableViewRecentTransactions->setSelectionBehavior(QAbstractItemView::SelectRows);
+                ui->tableViewRecentTransactions->setSelectionMode(QAbstractItemView::ExtendedSelection);
+                ui->tableViewRecentTransactions->setSortingEnabled(true);
+                ui->tableViewRecentTransactions->sortByColumn(TransactionTableModel::Date, Qt::DescendingOrder);
+                ui->tableViewRecentTransactions->verticalHeader()->hide();
+
+                ui->tableViewRecentTransactions->setModel(transactionProxyModel);
+              //  ui->tableViewRecentTransactions->setModelColumn(TransactionTableModel::ToAddicons);
+        //        transactionView->horizontalHeader()->resizeSection(TransactionTableModel::Status, 23);
+                ui->tableViewRecentTransactions->horizontalHeader()->resizeSection(TransactionTableModel::Date, 120);
+                ui->tableViewRecentTransactions->horizontalHeader()->resizeSection(TransactionTableModel::Type, 120);
+                ui->tableViewRecentTransactions->horizontalHeader()->setResizeMode(TransactionTableModel::ToAddress, QHeaderView::Stretch);
+                ui->tableViewRecentTransactions->horizontalHeader()->resizeSection(TransactionTableModel::Amount, 100);        // Keep up to date with wallet
+                setBalance(model->getBalance(), model->getStake(), model->getUnconfirmedBalance(), model->getImmatureBalance());
+                connect(model, SIGNAL(balanceChanged(qint64, qint64, qint64, qint64)), this, SLOT(setBalance(qint64, qint64, qint64, qint64)));
+
+                connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
+
+    }
+
+    // update the display unit, to not use the default ("BTC")
+    updateDisplayUnit();
+}
+
+void OverviewPage::updateDisplayUnit()
+{
+    if(walletModel && walletModel->getOptionsModel())
+    {
+        if(currentBalance != -1)
+            setBalance(currentBalance, walletModel->getStake(), currentUnconfirmedBalance, currentImmatureBalance);
+
+        // Update txdelegate->unit with the current unit
+        txdelegate->unit = walletModel->getOptionsModel()->getDisplayUnit();
+
+       ui->tableViewRecentTransactions->update();
+    }
+}
+
+void OverviewPage::updateAlerts(const QString &warnings)
+{
+    this->ui->labelAlerts->setVisible(!warnings.isEmpty());
+    this->ui->labelAlerts->setText(warnings);
+}
+
+void OverviewPage::showOutOfSyncWarning(bool fShow)
+{
+    ui->labelWalletStatus->setVisible(fShow);
+    ui->labelTransactionsStatus->setVisible(fShow);
+}
